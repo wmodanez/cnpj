451125904ddc4245ecd8fe7d11bb5116115803b7
Wesley Modanez <wesley.modanez@gmail.com>
2026-01-27 13:22:06 -0300

    Bump version to v4.1.0

--- cnpj_processor/__version__.py
+++ cnpj_processor/__version__.py
@@ -8,7 +8,7 @@ import subprocess
 
 __title__ = "CNPJ Processor"
 __author__ = "Wesley Modanez Freitas"
-__version__ = "4.0.9"
+__version__ = "4.1.0"
 __license__ = "MIT"
 
 def get_version():
@@ -36,7 +36,7 @@ def get_version():
 try:
     __version__ = get_version()
 except RuntimeError:
-    __version__ = "4.0.6"
+    __version__ = "4.1.0"
 
 def get_full_description():
     """Retorna a descrição completa da API."""
