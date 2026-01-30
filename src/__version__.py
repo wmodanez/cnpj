21f2c1f5c2e938b1425aeb86a3ea91cf5be8209c
Wesley Modanez <wesley.modanez@gmail.com>
2026-01-30 12:04:31 -0300

    Bump version to v4.1.3

--- src/__version__.py
+++ src/__version__.py
@@ -8,7 +8,7 @@ import subprocess
 import os
 
 __title__ = "Sistema de Processamento de Dados CNPJ"
-__version_fallback__ = "4.1.2"
+__version_fallback__ = "4.1.3"
 
 def get_git_version():
     """
